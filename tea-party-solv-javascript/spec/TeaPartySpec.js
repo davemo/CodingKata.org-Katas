845add37823f8972ed90f7b040ecff2d2af4fc98
David Mosher <davidmosher@gmail.com>
2011-04-20 23:11:20 -0600

    red on knighting

--- tea-party-solv-javascript/spec/TeaPartySpec.js
+++ tea-party-solv-javascript/spec/TeaPartySpec.js
@@ -4,23 +4,24 @@ describe("TeaParty", function() {
   sut = TeaParty.welcome;
   
   describe("welcoming a woman", function() {
-    
     it("should include the 'Ms.' prefix", function() {
         result = sut("Austen", true, false);
         expect(result).toContain("Ms.");
     });
-
   });
   
   describe("welcoming a man", function() {
-    it("should includ the 'Mr.' prefix", function() {
+    it("should include the 'Mr.' prefix", function() {
       result = sut("Baldwin", false, false);
       expect(result).toContain("Mr.");
     });
   });
   
-  describe("description", function() {
-    
+  describe("welcoming someone who has been knighted", function() {
+    it("should include the 'Sir' prefix", function() {
+      result = sut("Newton", false, true);
+      expect(result).toContain("Sir");
+    });
   });
   
 });
