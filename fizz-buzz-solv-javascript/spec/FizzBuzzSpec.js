4277019cd2af2b7a72fb658bcf654d7927c4a626
David Mosher <davidmosher@gmail.com>
2011-04-20 22:42:32 -0600

    red on number divisible by 5 returns buzz

--- fizz-buzz-solv-javascript/spec/FizzBuzzSpec.js
+++ fizz-buzz-solv-javascript/spec/FizzBuzzSpec.js
@@ -3,12 +3,17 @@ describe("fizzBuzz", function() {
   var sut = fizzBuzz;        
         
   describe("a number divisible by 3", function() {
-    
     it("returns the string 'fizz'", function() {
       result = sut(3);
       expect(result).toBe('fizz');
     });
-    
+  });
+  
+  describe("a number divisible by 5", function() {
+    it("returns the string 'buzz'", function() {
+      result = sut(5);      
+      expect(result).toBe('buzz');
+    });
   });
 
   
