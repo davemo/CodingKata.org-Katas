b524607094ad8d8a3211a12720cb11d5bd9b0123
David Mosher <davidmosher@gmail.com>
2011-04-20 22:46:44 -0600

    green on all 3

--- fizz-buzz-solv-javascript/src/FizzBuzz.js
+++ fizz-buzz-solv-javascript/src/FizzBuzz.js
@@ -1,5 +1,7 @@
 fizzBuzz = function(num) {
-    if(num % 3 === 0) {
+    if(num % 3 === 0 && num %5 === 0) {
+      return 'fizzbuzz';
+    } else if (num % 3 === 0){
       return 'fizz';
     } else {
       return 'buzz';
